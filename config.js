15826c6dca5b472f850b56b21de041697b68ef92
ToggyO <togushakov@gmail.com>
2019-11-06 23:26:48 +0700

    .env fixed

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const root = path.join.bind(this, __dirname);
 dotenv.config({ path: root('.env') });
 
 module.exports = {
+	NODE_ENV: process.env.NODE_ENV,
 	host: process.env.HOST,
 	port: process.env.PORT || 3001,
 	mongoURI: process.env.MONGO_URL,
